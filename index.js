6c3252ab2f8cd62f142016ae76ae76e72156b52d
Chris-Owusu <christopher.owusu@amalitech.com>
2023-01-17 11:40:24 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// kelvin variable
+let kelvin = 340;
+
+// convert kelvin to celcius
+celsius = kelvin - 273
+
+// calculation for fahrenheit with floor method
+fahrenheit = Math.floor(celsius * (9/5) + 32)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+// convert celsius to newton
+let newton = Math.floor(Celsius * (33/100))
+console.log(`The temperature is ${newton} Newton.`)
